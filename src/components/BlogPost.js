8b107b18d17df12a965e0398143045ca1885783b
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-13 21:54:14 +0100

    Update BlogPost.js

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles/BlogPost.css';
 
+const BlogPost = ({ post }) => {
+  return (
+    <div className="blog-post">
+      <h2>{post.title}</h2>
+      <p>{post.excerpt}</p>
+      <Link to={`/post/${post.id}`}>Read More</Link>
+    </div>
+  );
+};
+
+export default BlogPost;
