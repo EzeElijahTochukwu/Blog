664ced8238d55aa4d148c0ee1f0c9a03e37c0955
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-13 21:53:28 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles/Header.css';
 
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <Link to="/" className="logo">My Blog</Link>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/create">New Post</Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
