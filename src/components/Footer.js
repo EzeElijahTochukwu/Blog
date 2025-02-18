3e04586053e29f47fa325bc9a2073aff06f750b2
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-18 20:36:39 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,15 @@ const Footer = () => {
         <p>Phone: (123) 456-7890</p>
         <p>Address: 123 Blog Street, Blog City</p>
       </div>
+      <div className="links-section">
+        <h4>Quick Links</h4>
+        <ul>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/blog">Blog</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </div>
     </footer>
   );
 };
