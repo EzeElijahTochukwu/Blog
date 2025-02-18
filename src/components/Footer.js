1b67606cdaf8ac98742a2e3af176156d965b38b3
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-18 20:35:29 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,15 @@ const Footer = () => {
   return (
     <footer>
       <p>&copy; 2023 My Blog. All rights reserved.</p>
+      <div className="contact-section">
+        <h4>Contact Us</h4>
+        <p>Email: contact@myblog.com</p>
+        <p>Phone: (123) 456-7890</p>
+        <p>Address: 123 Blog Street, Blog City</p>
+      </div>
     </footer>
   );
 };
 
 export default Footer;
+  
